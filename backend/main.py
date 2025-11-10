94fc9bf03fa90029a4c357e75a3a75d18c8bcc6e
Wetli Jesse Ryan <jesseryan@bluewin.ch>
2025-11-10 14:19:32 +0100

    implement user signup and signin functionality with Supabase

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from supabase_handler.supabase_handler import supabase_handler
 
 app = FastAPI()
 
 supabase: supabase_handler = supabase_handler()
 
-supabase_client = supabase.get_supabase_client()
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/signup/")
+def sign_up(email: str, password: str):
+    response = supabase.sign_up_user(email, password)
+    return response
+
+@app.post("/signin/")
+def sign_in(email: str, password: str):
+    response = supabase.sign_in_user(email, password)
+    return response
--- backend/supabase_handler/supabase_handler.py
+++ backend/supabase_handler/supabase_handler.py
@@ -13,3 +13,17 @@ class supabase_handler:
 
     def get_supabase_client(self) -> Client:
         return self.supabase
+
+    def sign_up_user(self, email: str, password: str):
+        auth_response = self.supabase.auth.sign_up({
+            "email": email,
+            "password": password
+        })
+        return auth_response
+
+    def sign_in_user(self, email: str, password: str):
+        auth_response = self.supabase.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
+        return auth_response
